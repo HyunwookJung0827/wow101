2da725fd3070cec937ed14014142c44c3e9783b0
HyunwookJung0827 <as9804@berkeley.edu>
2024-02-21 15:48:05 -0800

    Next.js Tutorial - 12 - Custom 404 Page

--- next-routing/pages/index.tsx
+++ next-routing/pages/index.tsx
@@ -5,7 +5,8 @@ const Home = () => {
     const router = useRouter();
     const handleClick = () => {
         console.log('Placing your order');
-        router.replace('/product')
+        router.push('/product');
+        router.replace('/product');
     }
   return (<div>
     <h1>Home Page</h1><div>

380964265bcf04e7a84c501272e08ecb860e5df1
HyunwookJung0827 <as9804@berkeley.edu>
2024-02-21 15:44:47 -0800

    Next.js Tutorial - 11 - Navigating Programmatically

--- next-routing/pages/index.tsx
+++ next-routing/pages/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 const Home = () => {
+    const router = useRouter();
+    const handleClick = () => {
+        console.log('Placing your order');
+        router.replace('/product')
+    }
   return (<div>
     <h1>Home Page</h1><div>
     <Link href="/blog">Blog</Link></div><div>
     <Link href="/product">Products</Link></div>
+    <button onClick={handleClick}>
+        Place Order
+    </button>
     </div>
   )
 }

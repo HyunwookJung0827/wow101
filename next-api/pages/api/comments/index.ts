347ee5174cea50c47ba91387f93f698977843381
HyunwookJung0827 <as9804@berkeley.edu>
2024-02-21 21:03:15 -0800

    Next.js Tutorial - 43 - API POST Request

--- next-api/pages/api/comments/index.ts
+++ next-api/pages/api/comments/index.ts
@@ -1,5 +1,17 @@
 import { comments } from "@/data/comments"
 
 export default function handler(req, res) {
+    if (req.method === 'GET') {
     res.status(200).json(comments)
+    }
+    else if (req.method === 'POST') {
+        const comment = req.body.comment;
+        const newComment = {
+            id: Date.now(),
+            text: comment
+        }
+        // Store the generated comment in /data/comments.ts
+        comments.push(newComment);
+        res.status(201).json(newComment);
+    }
 }

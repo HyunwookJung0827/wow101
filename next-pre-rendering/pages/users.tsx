aaf99835602e984b045875eed674e0321a8f3af6
HyunwookJung0827 <as9804@berkeley.edu>
2024-02-23 10:13:07 -0800

    format

--- next-pre-rendering/pages/users.tsx
+++ next-pre-rendering/pages/users.tsx
@@ -1,31 +1,34 @@
-import User from '@/components/user'
-import React from 'react'
+import User from "@/components/user";
+import React from "react";
 interface User {
-  id: number,
-  name: string,
-  email: string
+  id: number;
+  name: string;
+  email: string;
 }
-const UserList = ({users}: User[]) => {
-  return <><h1>List of users</h1>
-  {
-    users.map((user: User) => {
-      return (<div key={user.id}>
-<User user={user}/>
-      </div>)
-    })
-  }</>
-    
-  
-}
+const UserList = ({ users }: { users: User[] }) => {
+  return (
+    <>
+      <h1>List of users</h1>
+      {users.map((user: User) => {
+        return (
+          <div key={user.id}>
+            <User user={user} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-export default UserList
+export default UserList;
 export async function getStaticProps() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  // convention is to return an object with a props key
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
   console.log(data);
   return {
     props: {
-      users: data
-    }
-  }
+      users: data,
+    },
+  };
 }

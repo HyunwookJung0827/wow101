bbd66d4af4254f3d5d68c6dc28f196fe7bbc462d
HyunwookJung0827 <as9804@berkeley.edu>
2024-02-23 09:45:19 -0800

    prettier integrated

--- assignment-1-20240221/pages/posts/[postId]/index.tsx
+++ assignment-1-20240221/pages/posts/[postId]/index.tsx
@@ -1,46 +1,54 @@
-import { useRouter
- } from "next/router"
+import { useRouter } from "next/router";
 import { useState } from "react";
 const PostDetail = () => {
   const [post, setPost] = useState({
-    "postId": null,
-    "id": null,
-    "title": "",
-    "body": ""
-});
-const [comments, setComments] = useState([{
-  "postId": null,
-  "id": null,
-  "name": "",
-  "email": "",
-  "body": ""
-}]);
-    const router = useRouter();
-    const postId = router.query.postId;
-    const fetchPost = async () => {
-      const response = await fetch(`/api/posts/${postId}`);
-      const data = await response.json();
-      setPost(data);
-    }
-    const fetchComments = async () => {
-      const response = await fetch(`/api/posts/${postId}/comments`);
-      const data = await response.json();
-      setComments(data);
-    }
-    return (<><button onClick={fetchPost}>Load Posts {postId}</button>
-      
-           
-              <div>
-                  <p>id: {post.id}</p><p>title: {post.title}</p><p>body: {post.body}</p>
-              </div>
-              <button onClick={fetchComments}>Load Comments</button>
-              {
-              comments.map((comment)=> {
-                return <div key={comment.id}>({comment.email}): {comment.body}</div>
-              })}
-          
-      
-  </>)
-}
+    postId: null,
+    id: null,
+    title: "",
+    body: "",
+  });
+  const [comments, setComments] = useState([
+    {
+      postId: null,
+      id: null,
+      name: "",
+      email: "",
+      body: "",
+    },
+  ]);
+  const router = useRouter();
+  const postId = router.query.postId;
+  const fetchPost = async () => {
+    const response = await fetch(`/api/posts/${postId}`);
+    const data = await response.json();
+    setPost(data);
+  };
+  const fetchComments = async () => {
+    const response = await fetch(`/api/posts/${postId}/comments`);
+    const data = await response.json();
+    setComments(data);
+  };
+  return (
+    <>
+      <button onClick={fetchPost}>Load Posts {postId}</button>
 
-export default PostDetail
+      <div>
+        <p>id: {post.id}</p>
+        <p>title: {post.title}</p>
+        <p>body: {post.body}</p>
+      </div>
+
+      <button onClick={fetchComments}>Load Comments</button>
+      
+      {comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            ({comment.email}): {comment.body}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default PostDetail;
--- assignment-1-20240221/pages/posts/index.tsx
+++ assignment-1-20240221/pages/posts/index.tsx
@@ -1,24 +1,32 @@
 import Link from "next/link";
 import React from "react";
 import { useState } from "react";
-
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
 const PostsPage = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
   const fetchPosts = async () => {
-    const response = await fetch('/api/posts');
+    const response = await fetch("/api/posts");
     const data = await response.json();
     setPosts(data);
-  }
+  };
 
-  return (<><button onClick={fetchPosts}>Load Posts</button>
-  {
-    posts.map((post) => {
+  return (
+    <>
+      <button onClick={fetchPosts}>Load Posts</button>
+      {posts.map((post) => {
         return (
-            <div key={post.id}><Link href={`/posts/${post.id}`}>
-                {post.id} {post.title}</Link>
-            </div>
-        )
-    })
-}</>)
-}
-export default PostsPage
+          <div key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              {post.id} {post.title}
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+export default PostsPage;

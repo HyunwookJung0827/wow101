0eacf37456c71c657c30fb47c50dd2b3394923e2
HyunwookJung0827 <as9804@berkeley.edu>
2024-03-06 20:34:27 -0800

    I have made 1000 tags and accounts, and some of them work on either localhost or vercel.app

--- 2-assignment-20240226/pages/_app.tsx
+++ 2-assignment-20240226/pages/_app.tsx
@@ -1,29 +1,17 @@
-import "@/styles/globals.css";
-// pages/_app.tsx
-
-import { AppProps } from 'next/app';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import TagManager from 'react-gtm-module';
-import { gtmConfig } from '../utils/gtm';
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+import TagManager, { TagManagerArgs } from "react-gtm-module";
+import { gtmConfig } from "../utils/gtm";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "";
 
+  const tagManagerArgs: TagManagerArgs = {
+    gtmId,
+  };
   useEffect(() => {
-    TagManager.initialize(gtmConfig);
-    const handleRouteChange = (url: string) => {
-      TagManager.dataLayer({
-        dataLayer: {
-          page: url,
-        },
-      });
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+    TagManager.initialize(tagManagerArgs);
+  }, []);
 
   return <Component {...pageProps} />;
 }
